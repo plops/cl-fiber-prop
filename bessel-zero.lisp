95e923722ef369a210f09bcfc50d774e5293536b
martin <martin@540m>
2013-09-06 12:47:41 +0200

    write a macro that generates interpolator for functions

--- bessel-zero.lisp
+++ bessel-zero.lisp
@@ -370,113 +370,64 @@ covers -scale*R .. scale*R and still ensures sampling of the signal"
 
 
 (defun bessel-j-and-deriv (l x)
-  (values (jn l x)
+  (declare (values single-float single-float &optional)
+	   (type fixnum l)
+	   (type single-float x))
+  (values (jnf l x)
 	  (cond 
-	    ((= l 0) (* .5 (- (jn 2 x) (jn 0 x))))
-	    ((= l 1) (* .25 (- (jn 3 x) (* 3 (jn 1 x)))))
-	    (t (* .25 (+ (jn (+ l 2) x)
-			 (* -2 (jn l x))
-			 (jn (- l 2) x)))))))
+	    ((= l 0) (* .5s0 (- (jnf 2 x) (jnf 0 x))))
+	    ((= l 1) (* .25s0 (- (jnf 3 x) (* 3 (jnf 1 x)))))
+	    (t (* .25s0 (+ (jnf (+ l 2) x)
+			   (* -2 (jnf l x))
+			   (jnf (- l 2) x)))))))
 
-(defclass cubic-interp ()
-  ((table :reader table :type (simple-array double-float 3)) ;; lmax points 2
-   (start :reader start :initarg :start :initform 0d0 :type double-float)
-   (end :reader end :initarg :end :initform 100d0 :type double-float)))
+(defmacro def-interp (name fun)
+  `(let* ((j-n 100)
+	  (lmax 10)
+	  (j-start 0s0)
+	  (j-end 100s0)
+	  (j (make-array (list lmax j-n 2) :element-type 'single-float
+			 :initial-element 0s0)))
+     (declare (type single-float j-start j-end)
+	      (type fixnum lmax j-n)
+	      (type (simple-array single-float 3) j))
+     (defun ,(intern (format nil "~a-INTERP-INIT" name))
+	 (&key (start 0s0) (end 10s0) (n 100))
+       (declare (optimize (debug 3) (speed 3) (safety 1)))
+       (setf j-start start
+	     j-end end
+	     j-n n
+	     j (make-array (list lmax j-n 2) :element-type 'single-float
+			   :initial-element 0s0))
+       (dotimes (l lmax)
+	 (dotimes (i n)
+	   (let ((x (+ start (* (- end start) i (/ 1s0 n)))))
+	     (declare (type single-float x))
+	     (multiple-value-bind (y yy) (,fun l x)
+	       (setf (aref j l i 0) y
+		     (aref j l i 1) yy))))))
+     (defun ,(intern (format nil "~a-INTERP" name)) (l x)
+       (declare (type (integer 0 10000) l)
+		(type single-float x)
+		(values single-float &optional))
+       (declare (optimize (debug 0) (speed 3) (safety 0)))
+       (multiple-value-bind (i xx) (floor (* j-n (/ (- x j-start)
+						    (- j-end j-start))))
 
-(defmethod initialize-instance :after ((c cubic-interp) &key (fun #'bessel-j-and-deriv) (lmax 10) (n 100))
-  (with-slots (start end table) c
-    (setf table (make-array (list lmax n 2) :element-type 'double-float))
-    (dotimes (l lmax)
-     (dotimes (i n)
-       (let ((x (+ start (* (- end start) i (/ 1d0 n)))))
-	 (multiple-value-bind (y yy) (funcall fun l x)
-	     (setf (aref table l i 0) y
-		   (aref table l i 1) yy)))))))
-#+nil
-(defparameter *bla* (make-instance 'cubic-interp))
+	 (declare (type (signed-byte 64) i)
+		  (type single-float xx))
+	 (let* ((diff (* (- j-end j-start)
+			 (/ 1s0 j-n)))
+		(a (- 1 xx))
+		(b xx)
+		(c (* 1/6 (- (* a a a) a) ))
+		(d (* 1/6 (- (* b b b) b) )))
+	   (+ (* a  (aref j l i 0))
+	      (* b (aref j l (+ i 1) 0))
+	      (* c (aref j l i 1) diff diff)
+	      (* d (aref j l (+ i 1) 1) diff diff)))))))
 
-#+nil
-(array-dimensions (slot-value *bla* 'table))
-
-
-(defmethod interp ((lut cubic-interp) l x)
-    (declare (type fixnum l)
-	     (type double-float x)
-	     (values double-float &optional))
-    (declare (optimize (debug 0) (speed 3) (safety 1)))
-    (with-slots (start end table) lut
-      (declare (type double-float start end)
-	       (type (simple-array double-float 3) table))
-      (destructuring-bind (lmax n two) (array-dimensions table)
-	(declare (type fixnum lmax n)
-		 (ignore two))
-	(multiple-value-bind (i xx) (floor (* n (/ (- x start)
-						   (- end start))))
-	  (declare (type double-float xx)
-		   (type fixnum i))
-	  (let* ((diff (* (- end start) (/ 1d0 n)))
-		  (a (- 1 xx))
-		  (b xx)
-		  (c (* 1/6 (- (* a a a) a)))
-		  (d (* 1/6 (- (* b b b) b))))
-	    (declare (type double-float diff a b c d))
-	     (+ (* a (aref table l i 0))
-		(* b (aref table l (+ i 1) 0))
-		(* c (aref table l i 1) diff diff)
-		(* d (aref table l (+ i 1) 1) diff diff)))))))
-
-#+nil
-(let ((j (make-instance 'cubic-interp)))
-  (interp j 3 .3))
-
-(let* ((j-n 100)
-       (lmax 10)
-       (j-start 0s0)
-       (j-end 100s0)
-       (j (make-array (list lmax j-n 2) :element-type 'single-float
-		      :initial-element 0s0)))
-  (declare (type single-float j-start j-end)
-	   (type fixnum lmax j-n)
-	   (type (simple-array single-float 3) j))
-  (defun bessel-j-fast-init (&key (start 0s0) (end 10s0) (n 100))
-    (declare (optimize (debug 3) (speed 3) (safety 1)))
-    (setf j-start start
-	  j-end end
-	  j-n n
-	  j (make-array (list lmax j-n 2) :element-type 'single-float
-			:initial-element 0s0))
-    (dotimes (l lmax)
-     (dotimes (i n)
-       (let ((x (+ start (* (- end start) i (/ 1s0 n)))))
-	 (declare (type single-float x))
-	(setf (aref j l i 0) (jnf l x)
-	      (aref j l i 1) (cond 
-			       ((= l 0) (* .5s0 (- (jnf 2 x) (jnf 0 x))))
-			       ((= l 1) (* .25s0 (- (jnf 3 x) (* 3 (jnf 1 x)))))
-			       (t (* .25s0 (+ (jnf (+ l 2) x)
-					      (* -2 (jnf l x))
-					      (jnf (- l 2) x))))))))))
-  (defun bessel-j-fast (l x)
-    (declare (type (integer 0 10000) l)
-	     (type single-float x)
-	     (values single-float &optional))
-    (declare (optimize (debug 0) (speed 3) (safety 1)))
-    (multiple-value-bind (i xx) (floor (* j-n (/ (- x j-start)
-						 (- j-end j-start))))
-
-      (declare (type (signed-byte 64) i)
-	       (type single-float xx))
-      (let* ((diff (* (- j-end j-start)
-		      (/ 1s0 j-n)))
-	     (a (- 1 xx))
-	     (b xx)
-	     (c (* 1/6 (- (* a a a) a) ))
-	     (d (* 1/6 (- (* b b b) b) )))
-	(+ (* a  (aref j l i 0))
-	   (* b (aref j l (+ i 1) 0))
-	   (* c (aref j l i 1) diff diff)
-	   (* d (aref j l (+ i 1) 1) diff diff))))))
-
+(def-interp bessel-j bessel-j-and-deriv)
 
 #+nil
 (let ((j (make-instance 'cubic-interp)))
@@ -486,15 +437,9 @@ covers -scale*R .. scale*R and still ensures sampling of the signal"
 	(format s "~f ~f~%" x (+ (* -1 (jnf 4 x)) #+nil (interp j 4 x) (* 0 (bessel-j-fast 4 x)))))))
 
 #+nil
-(let ((j (make-instance 'cubic-interp)))
-  (time
-   (loop for x from 0d0 upto 99d0 by 1e-5 do (interp j 4 .3))))
-; (/ 25558000456 (* 99 1e5)) => 2581 cycles per call
-; (/ 12835490708 (* 99 1e5)) => 1296 cycles per call
-#+nil
 (progn
-  (bessel-j-fast-init :start 0s0 :end 110s0 :n 100)
-  (time (loop for x from 0s0 upto 99s0 by 1s-5 do (bessel-j-fast 4 x))))
+  (bessel-j-interp-init :start 0s0 :end 110s0 :n 100)
+  (time (loop for x from 0s0 upto 99s0 by 1s-5 do (bessel-j-interp 4 x))))
 ; (/ 1815165956 (* 99 1e5)) => 183 cycles per call
 ; (/ 1386921624 (* 99 1e5)) => 140 cycles per call (single-float)
 
