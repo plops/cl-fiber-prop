b8d977e522ecd5484e2500e6abbcce796dada427
Martin Kielhorn <kielhorn.martin@gmail.com>
2013-10-20 02:00:45 +0000

    simulation starts to resemble something that i would expect

--- step-coupling.lisp
+++ step-coupling.lisp
@@ -7,10 +7,13 @@
 
 (in-package :cl-fiber-prop)
 
-(defun k-mu-nu (u-modes &key (v 10d0) (wavelength .633d-3) (refractive-index 1.5d0) (rco 12d-3) (bend-radius 1d0))
+
+(defun k-mu-nu (u-modes &key (v 10d0) (wavelength .633d-3) (nco 1.43d0) (rco 12d-3) (bend-radius 1d0))
  (let* ((k (/ (* 2 pi) wavelength))
+	(ncl (sqrt (- (expt nco 2)
+		      (expt (/ V (* k rco)) 2))))
 	(u-lin (step-fiber-eigenvalues-linear u-modes))
-	(b-lin (step-fiber-betas-linear u-lin v :lambd wavelength))
+	(b-lin (step-fiber-betas-linear u-lin v :lambd wavelength :nco nco :bigdelta (bigdelta nco ncl)))
 	(n (number-of-modes u-modes))
 	(kbar (make-array (list n n) :element-type 'double-float)))
    (loop for j below n do		; in document nu 
@@ -55,7 +58,7 @@
 					 (gsl:cylindrical-bessel-j alj uj)
 					 (gsl:cylindrical-bessel-j ali ui)
 					 (/ (sqrt (* e-mu e-nu)))
-					 (/ (* 2 refractive-index k (expt rco 3)))
+					 (/ (* 2 ncl k (expt rco 3)))
 					 (/ (sqrt (abs (* (gsl:cylindrical-bessel-j (- alj 1) uj)
 							  (gsl:cylindrical-bessel-j (+ alj 1) uj)
 							  (gsl:cylindrical-bessel-j (- ali 1) ui)
@@ -65,10 +68,13 @@
 
 (defparameter *u-modes* (step-fiber-eigenvalues 10d0))
 
-(defparameter *bla* (k-mu-nu *u-modes*))
+(defparameter *k-mu-nu* (k-mu-nu *u-modes*))
+
+(let ((v 10d0))
+ (defparameter *b-lin* (step-fiber-betas-linear (step-fiber-eigenvalues-linear *u-modes*) v :lambd .633e-3)))
 
 (progn (terpri)
- (destructuring-bind (h w) (array-dimensions *bla*)
+ (destructuring-bind (h w) (array-dimensions *k-mu-nu*)
    (dotimes (j h)
      (dotimes (i w)
        (let ((val (aref *bla* j i)))
@@ -78,43 +84,51 @@
      (terpri))))
 
 
-
-(defparameter *terms*
-  (let* ((res nil)
-	 (u-lin (step-fiber-eigenvalues-linear *u-modes*))
-	 (b-lin (step-fiber-betas-linear u-lin 10d0 :lambd .633e-3)))
-    (destructuring-bind (h w) (array-dimensions *bla*)
-      (dotimes (i w)
-	(dotimes (j h)
-	  (unless (= 0d0 (aref *bla* j i))
-	    (push (list j i
-			(aref *bla* j i) 
-			(- (aref b-lin j) 
-			   (aref b-lin i))) res)))))
-    (reverse res)))
-
-(defun fun (z c cc)
-;  (format t "hallo~%")  (format t "~A~%" (list z (grid:aref c 0) (grid:aref cc 0)))
-  (flet ((ev-real (index-mu) 
-	   (loop for (j i k delb) in *terms* and index from 0 sum
-		(if (= index index-mu) 
-		    0d0
-		    (* k (grid:aref c (* 2 index)) (sin (* z delb))))))
-	 (ev-imag (index-mu) 
-	   (loop for (j i k delb) in *terms* and index from 0 sum
-		(if (= index index-mu) 
-		    0d0
-		    (* k (grid:aref c (+ 1 (* 2 index))) (- (cos (* z delb))))))))
-    (loop for index-mu below (grid:dim0 cc) do
-	 (setf (grid:aref cc index-mu) (if (evenp index-mu) 
-					  (ev-real index-mu)
-					  (ev-imag index-mu)))))
+(defun coupled-mode-equations (z c dcdz)
+  ;;  (format t "hallo~%")  (format t "~A~%" (list z (grid:aref c 0) (grid:aref cc 0)))
+  (let ((n (grid:dim0 c)))
+    (flet ((ev-real (mu) 
+	     (loop for nu below (floor n 2) sum 
+		  (* (aref *k-mu-nu* nu mu) (grid:aref c (* 2 nu)) (sin (* z (- (aref *b-lin* mu) (aref *b-lin* nu)))))))
+	   (ev-imag (mu)
+	     (loop for nu below (floor n 2) sum 
+		  (* (aref *k-mu-nu* nu mu) (grid:aref c (+ (* 2 nu) 1)) (- (cos (* z (- (aref *b-lin* mu) (aref *b-lin* nu)))))))))
+      (dotimes (mu n)
+	(setf (grid:aref dcdz mu) (if (evenp mu) 
+				     (ev-real (floor mu 2))
+				     (ev-imag (floor mu 2)))))))
   gsll::+success+)
 
 
+(defun coupled-mode-jacobian (z c dfdc dfdz)
+  (let ((n (grid:dim0 c)))
+    (flet ((ev-real (mu) 
+	     (loop for nu below (floor n 2) sum 
+		  (* (aref *k-mu-nu* nu mu) (grid:aref c (* 2 nu)) -1 (- (aref *b-lin* mu) (aref *b-lin* nu)) (cos (* z (aref *b-lin* nu))))))
+	   (ev-imag (mu)
+	     (loop for nu below (floor n 2) sum 
+		  (* (aref *k-mu-nu* nu mu) (grid:aref c (+ (* 2 nu) 1)) (- (aref *b-lin* mu) (aref *b-lin* nu)) (- (sin (* z (aref *b-lin* nu))))))))
+      (dotimes (mu n)
+	(setf (grid:aref dfdz mu) (if (evenp mu)
+				     (ev-real (floor mu 2))
+				     (ev-imag (floor mu 2))))))
+    
+    (dotimes (mu n)
+      (dotimes (nu n)
+	(setf (grid:aref dfdc mu nu) 0d0)))
+    
+    (dotimes (mu (floor n 2))
+      (dotimes (nu (floor n 2))
+	;; first mu then nu
+	(setf (grid:aref dfdc (* 2 mu) (* 2 nu))             (* (aref *k-mu-nu* nu mu) (sin (* z (- (aref *b-lin* mu) (aref *b-lin* nu)))))
+	      (grid:aref dfdc (+ 1 (* 2 mu)) (+ 1 (* 2 nu))) (* (aref *k-mu-nu* nu mu) (- (cos (* z (- (aref *b-lin* mu) (aref *b-lin* nu))))))))))
+  gsll::+success+)
+
+(grid:dim0 (grid:make-foreign-array 'double-float :dimensions '(3 2)))
 
 (defparameter *c* (gsll:make-standard-control 1d-6 1d-3 1d0 1d0))
 
+#+nil
 (defparameter *s*
   (destructuring-bind (h w) (array-dimensions *bla*)
     (gsll:make-ode-stepper gsll:+step-rk2+ (* w 2) #'fun :scalarsp nil)))
@@ -124,25 +138,27 @@
    (gsll:make-ode-evolution (* 2 w))))
 
 #+nil
-(destructuring-bind (h w) (array-dimensions *bla*)
-  (let ((y0 (grid:make-foreign-array 'double-float :dimensions (* 2 w)))
+(destructuring-bind (n) (array-dimensions *b-lin*)
+  (let ((y0 (grid:make-foreign-array 'double-float :dimensions (* 2 n)))
 	(time (grid:make-foreign-array 'double-float :dimensions 1))
 	(step-size (grid:make-foreign-array 'double-float :dimensions 1))
-	(ctl (gsll:make-standard-control 1d-1 1d-16 1d0 0d0))
-	(stepper (gsll:make-ode-stepper gsll:+step-rk8pd+ (* w 2) #'fun nil nil))
-	(evo (gsll:make-ode-evolution (* 2 w))))  
+	(ctl (gsll:make-standard-control 1d-8 1d-8 1d0 0d0))
+	(stepper (gsll:make-ode-stepper gsll:+step-bsimp+ (* n 2) #'coupled-mode-equations #'coupled-mode-jacobian nil))
+	(evo (gsll:make-ode-evolution (* 2 n)))
+	(max-time 20d0))  
     (loop for i below (grid:dim0 y0) do (setf (grid:aref y0 i) 0d0))
     (setf (grid:aref y0 0) 1d0
 	  (grid:aref time 0) 0d0
-	  (grid:aref step-size 0) 1d-7)
-    (loop while (and (< (grid:aref time 0) 2d0)
+	  (grid:aref step-size 0) 1d-2)
+    (terpri)
+    (loop while (and (< (grid:aref time 0) max-time)
 		     (< (abs (complex (grid:aref y0 (* 2 1)) 
 				      (grid:aref y0 (+ 1 (* 2 1))))) 1d0)) do
-	 (gsll:apply-evolution evo time y0 step-size ctl stepper 2d0)
-	 (format t "~20,12f ~6,3g ~{~18,13f ~}~%" 
+	 (gsll:apply-evolution evo time y0 step-size ctl stepper max-time)
+	 (format t "~20,12f ~8,3g ~{~18,13f ~}~%" 
 		 (grid:aref time 0)
 		 (grid:aref step-size 0)
-		 (loop for i below 3 collect 
+		 (loop for i below 7 collect 
 		      (abs (complex (grid:aref y0 (* 2 i)) 
 				    (grid:aref y0 (+ 1 (* 2 i))))))))))
 
