1d6a637ac59b757e26e264c04d4329ce88849f8c
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-10-14 12:20:07 +0200

    create a argb32 array in lisp and use it to fill the background of the clock widget

--- gtkclock.lisp
+++ gtkclock.lisp
@@ -12,7 +12,19 @@
 	 :accessor clock-face-time))
   (:metaclass gobject-class))
 
-(cairo-image-surface-create-from-png )
+(defun surface-from-lisp-array (w h)
+  (let* ((format :argb32)
+	 (stride (cairo-format-stride-for-width format w))
+	 (a (make-array (list h stride) :element-type '(unsigned-byte 8))))
+    (dotimes (j h)
+      (dotimes (i w)
+	(setf (aref a j (+ 0 (* 4 i))) (min 255 (max 0 i))
+	      (aref a j (+ 1 (* 4 i))) (min 255 (max 0 i))
+	      (aref a j (+ 2 (* 4 i))) (min 255 (max 0 j))
+	      (aref a j (+ 3 (* 4 i))) (min 255 (max 0 j)))))
+    (cairo-image-surface-create-for-data 
+       (sb-sys:vector-sap (sb-ext:array-storage-vector a))
+       format w h stride)))
 
 (defmethod initialize-instance :after ((clock clock-face) &key &allow-other-keys)
   (g-timeout-add 1000 (lambda ()
@@ -23,8 +35,10 @@
   (g-signal-connect clock "draw"
 		    (lambda (widget cr)
 		      (let ((cr (pointer cr))
-			    (window (gtk-widget-window widget)))
+			    (window (gtk-widget-window widget))
+			    (surf (surface-from-lisp-array 128 128)))
 			(cairo-set-source-rgb cr 1.0 1.0 1.0)
+			(cairo-set-source-surface cr surf 0 0)
 			(cairo-paint cr)
 			(let* ((x (/ (gdk-window-get-width window) 2))
 			       (y (/ (gdk-window-get-height window) 2))
@@ -44,16 +58,16 @@
 			    (cairo-stroke cr)
 			    (cairo-restore cr)))
 			
+			(cairo-surface-destroy surf)
 			(cairo-destroy cr)
 			t))))
-
-
 (defun run ()
   (sb-int:with-float-traps-masked (:divide-by-zero)
    (within-main-loop
      (let ((window (make-instance 'gtk-window :title "clock"
 				  :default-width 128
-				  :default-height 128))
+				  :default-height 128
+				  ))
 	   (clock (make-instance 'clock-face)))
        (g-signal-connect window "destroy"
 			 (lambda (widget) (leave-gtk-main)))
