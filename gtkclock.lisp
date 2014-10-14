0382f961c7fbc6a682579ada91ef2e687decfa2f
Martin Kielhorn <kielhorn.martin@gmail.com>
2014-10-14 15:27:48 +0200

    it is now possible to update the image while gtk is running

--- gtkclock.lisp
+++ gtkclock.lisp
@@ -12,19 +12,33 @@
 	 :accessor clock-face-time))
   (:metaclass gobject-class))
 
-(defun surface-from-lisp-array (w h)
-  (let* ((format :argb32)
-	 (stride (cairo-format-stride-for-width format w))
-	 (a (make-array (list h stride) :element-type '(unsigned-byte 8))))
-    (dotimes (j h)
-      (dotimes (i w)
-	(setf (aref a j (+ 0 (* 4 i))) (min 255 (max 0 i))
-	      (aref a j (+ 1 (* 4 i))) (min 255 (max 0 i))
-	      (aref a j (+ 2 (* 4 i))) (min 255 (max 0 j))
-	      (aref a j (+ 3 (* 4 i))) (min 255 (max 0 j)))))
-    (cairo-image-surface-create-for-data 
-       (sb-sys:vector-sap (sb-ext:array-storage-vector a))
-       format w h stride)))
+
+(let ((a (make-array (list 256 256) :element-type '(unsigned-byte 8))))
+  (dotimes (j 256)
+    (dotimes (i 256)
+      (setf (aref a j i) j)))
+  (update-img a)
+  nil)
+
+(let ((img nil))
+  (defun update-img (a)
+    (setf img a))
+  (defun surface-from-lisp-array ()
+    (when img
+      (destructuring-bind (h w) (array-dimensions img)
+	(let* ((format :argb32)
+	       (stride (cairo-format-stride-for-width format w))
+	       (a (make-array (list h stride) :element-type '(unsigned-byte 8))))
+	  (dotimes (j h)
+	    (dotimes (i w)
+	      (let ((v (max 0 (min 255 (aref img j i)))))
+	       (setf (aref a j (+ 0 (* 4 i))) v
+		     (aref a j (+ 1 (* 4 i))) v
+		     (aref a j (+ 2 (* 4 i))) v
+		     (aref a j (+ 3 (* 4 i))) v))))
+	  (cairo-image-surface-create-for-data 
+	  (sb-sys:vector-sap (sb-ext:array-storage-vector a))
+	  format w h stride))))))
 
 (defmethod initialize-instance :after ((clock clock-face) &key &allow-other-keys)
   (g-timeout-add 1000 (lambda ()
@@ -36,16 +50,17 @@
 		    (lambda (widget cr)
 		      (let ((cr (pointer cr))
 			    (window (gtk-widget-window widget))
-			    (surf (surface-from-lisp-array 128 128)))
+			    (surf (surface-from-lisp-array)))
 			(cairo-set-source-rgb cr 1.0 1.0 1.0)
-			(cairo-set-source-surface cr surf 0 0)
+			(when surf
+			 (cairo-set-source-surface cr surf 0 0))
 			(cairo-paint cr)
 			(let* ((x (/ (gdk-window-get-width window) 2))
 			       (y (/ (gdk-window-get-height window) 2))
 			       (radius (- (min x y) 12)))
 			  (cairo-arc cr x y radius 0 (* 2 pi))
-			  (cairo-set-source-rgb cr 1 1 1)
-			  (cairo-fill-preserve cr)
+			  ;(cairo-set-source-rgb cr 1 1 1)
+			  ;(cairo-fill-preserve cr)
 			  (cairo-set-source-rgb cr 0 0 0)
 			  (cairo-stroke cr)
 			  (let ((angle (* (/ pi 30) (first (clock-face-time clock)))))
@@ -58,15 +73,16 @@
 			    (cairo-stroke cr)
 			    (cairo-restore cr)))
 			
-			(cairo-surface-destroy surf)
+			(when surf
+			 (cairo-surface-destroy surf))
 			(cairo-destroy cr)
 			t))))
 (defun run ()
   (sb-int:with-float-traps-masked (:divide-by-zero)
    (within-main-loop
      (let ((window (make-instance 'gtk-window :title "clock"
-				  :default-width 128
-				  :default-height 128
+				  :default-width 512
+				  :default-height 512
 				  ))
 	   (clock (make-instance 'clock-face)))
        (g-signal-connect window "destroy"
